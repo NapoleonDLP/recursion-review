571d0891dcb2f1c389ec21c458faaa1bb15a44d5
Henry Mattoon <hmattoon@mail.sfsu.edu>
2019-05-28 16:32:57 -0700

    forgot to save changes before pushing!

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -7,19 +7,27 @@ var parseJSON = function(json) {
 
 
 
+
   //check first character
   //if first char is {
     //result = {}
+    //json = json - firstchar and -lastchard// '["one", "two"]'.slice(1, json.length-1) --> '"one", "two"'.split(', ') --> ['"one":value', '"two":value']
+    //loop through each value in array and then slit at (':') ---> ['"one"', 'value']
+    //result[parseJSON(array[x][0])] = parseJSON(array[x][1])
+
 
   //else if first char is [
     //result = []
     //json = json - firstchar and -lastchard// '["one", "two"]'.slice(1, json.length-1) --> '"one", "two"'.split(', ') --> ["one", "two"]
 
   //else if first char is "
+    //return json.slice(1, json.length -1)
 
-  //else if Number(json) !== NaN
+  //else if Number(json) !== NaN to check if number
+    //return Number(json)
 
   //else if json === "true" or "false"
+    //return !0 or !!0;
 
 
 
