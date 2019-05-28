094ef719777bf41bb9bd052645f067911740b0b0
Henry Mattoon <hmattoon@mail.sfsu.edu>
2019-05-28 16:29:22 -0700

    added a bunch of pseudocode to be finished later

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -4,4 +4,36 @@
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
   // your code goes here
+
+
+
+  //check first character
+  //if first char is {
+    //result = {}
+
+  //else if first char is [
+    //result = []
+    //json = json - firstchar and -lastchard// '["one", "two"]'.slice(1, json.length-1) --> '"one", "two"'.split(', ') --> ["one", "two"]
+
+  //else if first char is "
+
+  //else if Number(json) !== NaN
+
+  //else if json === "true" or "false"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 };
