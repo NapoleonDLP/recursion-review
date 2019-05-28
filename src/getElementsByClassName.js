9cf5cb9bb7c7c0ce96cb187e7ffa6699584b7e7a
Henry Mattoon <hmattoon@mail.sfsu.edu>
2019-05-28 14:53:51 -0700

    completed function so that it passed provided tests by checking current node and pushing to results, then recursivly calling itself on any child nodes  Please enter the commit message for your changes. Lines starting

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,40 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className, node) {
   // your code here
+
+  //input classname, node
+  //output array of elemenets
+  var results = [];
+
+  //if node is undefined
+  if(node === undefined){
+    //then we set node to be the body of the document
+    node = document.body;
+  }
+
+  if(node.className){
+    //create array of classes names from element if exists
+    var nodeClasses = node.className.split(' ');
+    //check if node had given class name
+    if(nodeClasses.includes(className)){
+      //if it did then push element to results
+      results.push(node);
+    }
+  }
+
+  //check if the node has any children
+  if(node.hasChildNodes()){
+    for(var i = 0; i < node.childNodes.length; i++){
+      //concat results with recursive call of getElementsByClassName on each child node
+      results = results.concat(getElementsByClassName(className, node.childNodes[i]));
+
+    }
+  }
+
+  return results;
+
+
+
 };
